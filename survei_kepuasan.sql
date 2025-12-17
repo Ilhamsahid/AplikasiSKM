4505f34aa8b85bd19ff8c543937abb457ba8d8fb
KuroFurtex <79640910+KuroFurtex@users.noreply.github.com>
2025-12-17 14:44:25 +0700

    Update survei_kepuasan.sql

--- survei_kepuasan.sql
+++ survei_kepuasan.sql
@@ -49,11 +49,29 @@ CREATE TABLE IF NOT EXISTS `tb_jawaban` (
 -- Dumping structure for table survei_kepuasan.tb_pertanyaan
 CREATE TABLE IF NOT EXISTS `tb_pertanyaan` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `pertanyaan` varchar(120) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `pertanyaan` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+  `tipe` enum('SelectOne','Date') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'SelectOne',
+  `jawaban` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table survei_kepuasan.tb_pertanyaan: ~0 rows (approximately)
+-- Dumping data for table survei_kepuasan.tb_pertanyaan: ~15 rows (approximately)
+INSERT INTO `tb_pertanyaan` (`id`, `pertanyaan`, `tipe`, `jawaban`) VALUES
+	(1, 'Apa jenis pelayanan yang pernah Saudara urusi?', 'SelectOne', 'Pelayanan Rekomendasi Ijin Praktik/Kerja Tenaga Kesehatan:Pelayanan Rekomendasi Ijin Fasilitas Kesehatan:Pelayanan Konsultasi PIRT:Pelayanan Fogging'),
+	(2, 'Kapan Saudara terakhir mengurus ijin untuk organisasi Saudara', 'Date', NULL),
+	(3, 'Kemudahan akses informasi layanan  Dinas Kesehatan Pengendalian Penduduk dan KB Kota Probolinggo. (informasi layanan tersedia di berbagai media elektronik dan nonelektronik)', 'SelectOne', 'Tidak Mudah:Kurang Mudah:Mudah:Sangat Mudah'),
+	(4, 'Kesesuaian persyaratan pelayanan dengan jenis pelayanannya', 'SelectOne', 'Tidak Sesuai:Kurang Sesuai:Sesuai:Sangat Sesuai'),
+	(5, 'Bagaimana pendapat Saudara tentang kemudahan Sistem, mekanisme dan prosedur pelayanan', 'SelectOne', 'Tidak Mudah:Kurang Mudah:Mudah:Sangat Mudah'),
+	(6, 'Bagaimana pendapat Saudara tentang kecepatan waktu dalam memberikan pelayanan?', 'SelectOne', NULL),
+	(7, 'Bagaimana pendapat Saudara tentang jam buka pelayanan sudah tepat waktu sesuai dengan Standar pelayanan yang dijanjikan?', 'SelectOne', NULL),
+	(8, 'Tarif atau biaya pelayanan yang diberikan sesuai dengan ketentuan', 'SelectOne', NULL),
+	(9, 'Bagaimana pendapat saudara tentang Kesesuaian produk pelayanan antara yang tercantum dalam standar pelayanan dengan hasil yang diberikan', 'SelectOne', NULL),
+	(10, 'Bagaimana pendapat saudara tentang penanganan pengaduan pengguna pelayanan', 'SelectOne', NULL),
+	(11, 'Bagaimana pendapat Saudara tentang kompetensi /kemampuan petugas dalam layanan?', 'SelectOne', NULL),
+	(12, 'Bagaimana perilaku petugas dalam layanan terkait dengan kesopanan dan keramahan?', 'SelectOne', NULL),
+	(13, 'Bagaimana pendapat Saudara tentang kualitas sarana dan prasarana pelayanan?', 'SelectOne', NULL),
+	(14, 'Bagaimana pendapat Saudara tentang transparansi pelayanan (informasi dan proses pelayanan transparan akuntabel)', 'SelectOne', NULL),
+	(15, 'Integritas petugas pelayanan, apakah berintegritas tinggi dan tidak melakukan KKN', 'SelectOne', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
