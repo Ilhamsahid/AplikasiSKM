71d05a12161ae3801e71027b96c662bcfba63f0b
Ilhamsahid <ilhamsahit988@gmail.com>
2026-01-25 19:41:52 +0700

    chore: format code app_helpers

--- app/helpers/app_helpers.php
+++ app/helpers/app_helpers.php
@@ -1,13 +1,14 @@
 <?php
 
-if(!function_exists('getView')){
-    function getView($view, $data=[]){
-        $base_path = __DIR__ . '/../view/';
-        $path = $base_path . str_replace('.', '/', $view) . '.php';
-        extract($data);
+if (!function_exists('getView')) {
+  function getView($view, $data = [])
+  {
+    $base_path = __DIR__ . '/../view/';
+    $path = $base_path . str_replace('.', '/', $view) . '.php';
+    extract($data);
 
-        ob_start();
-        include $path;
-        return ob_get_clean();
-    }
+    ob_start();
+    include $path;
+    return ob_get_clean();
+  }
 }
